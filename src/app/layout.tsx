6f902d79b1f4b2b17ec3559a5cb3cb47987daabc
Jang Haemin <io@jhaemin.com>
2024-11-01 00:48:29 +0900

    Update open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,18 @@ import './global-styles.css'
 export const metadata: Metadata = {
   title: 'Interop',
   description: '세상에서 가장 조화로운 한글 alphabet 고딕체',
+  openGraph: {
+    type: 'website',
+    url: 'https://interop.design',
+    title: 'Interop',
+    description: '세상에서 가장 조화로운 한글 alphabet 고딕체',
+    siteName: 'Interop',
+    images: [
+      {
+        url: 'https://interop.design/og.png',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
